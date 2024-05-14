17a0ecc30e5c8e53ab56532978f6468cd6cc8a93
yingqing001 <154562310+yingqing001@users.noreply.github.com>
2024-05-14 18:33:08 -0400

    Update aesthetic_scorer.py

--- aesthetic_scorer.py
+++ aesthetic_scorer.py
@@ -30,25 +30,33 @@ class AestheticScorerDiff(torch.nn.Module):
     def __init__(self, dtype=torch.float32):
         super().__init__()
         self.clip = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
         self.mlp = MLPDiff()
         state_dict = torch.load(ASSETS_PATH.joinpath("sac+logos+ava1-l14-linearMSE.pth"))
         self.mlp.load_state_dict(state_dict)
         self.dtype = dtype
         self.eval()
-        self.normalize = torchvision.transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
-                                                std=[0.26862954, 0.26130258, 0.27577711])
-
+                                               
     def __call__(self, images):
+        #target_size = 224
+        #normalize = torchvision.transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
+        #                                                   std=[0.26862954, 0.26130258, 0.27577711])
+        
+        if isinstance(images, torch.Tensor):
+            images = (images * 255).round().clamp(0, 255).to(torch.uint8)
+        else:
+            images = images.transpose(0, 3, 1, 2)  # NHWC -> NCHW
+            images = torch.tensor(images, dtype=torch.uint8)
+
         device = next(self.parameters()).device
-        target_size = 224
-
-        im_pix = ((images / 2) + 0.5).clamp(0, 1) 
-        im_pix = torchvision.transforms.Resize(target_size)(im_pix)
-        im_pix = self.normalize(im_pix).to(images.dtype)
-
-        embed = self.clip.get_image_features(pixel_values=im_pix)
+        inputs = self.processor(images=images, return_tensors="pt")
+        inputs = {k: v.to(self.dtype).to(device) for k, v in inputs.items()}
+        embed = self.clip.get_image_features(**inputs)
+        # normalize embedding
         embed = embed / torch.linalg.vector_norm(embed, dim=-1, keepdim=True)
         return self.mlp(embed).squeeze(1)
+
+        
     
 
 
