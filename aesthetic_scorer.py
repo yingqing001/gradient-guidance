be857f7b24401a70bee95cd983f7e548d79a7c58
yingqing001 <154562310+yingqing001@users.noreply.github.com>
2024-05-15 10:38:06 -0400

    Update aesthetic_scorer.py

--- aesthetic_scorer.py
+++ aesthetic_scorer.py
@@ -44,7 +44,10 @@ class AestheticScorerDiff(torch.nn.Module):
         
         if isinstance(images, torch.Tensor):
             print(images)
+            images = (images + 1) / 2.0
+            images = images.clamp(0, 1)
             images = (images * 255).round().clamp(0, 255).to(torch.uint8)
+            print(images)
             #print('-------------------')
             #print("images shape:")
             #print(images.shape)
